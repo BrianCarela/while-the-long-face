be6066c33bd5c5ce006d9d4ad33d9a34cf86fbc5
BrianCarela <briancarela@gmail.com>
2021-03-04 15:10:55 -0500

    a solution

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 function numberDoubler(num) {
     // Your code here
+
+    let output = num;
+    while (output <= 100){
+        output = output *2;
+}   
+        return output;
 }
 
 function stringRepeater(str) {
     // Your code here
+    let rep = "";
+    while (rep.length < 10){
+        rep += str;
+    }
+        return rep;
 }
 
 function makeDivisible(x, y) {
     // Your code here
+    while (x % y){
+        x+=1;
+    }
+    return x;
 }
 
 module.exports = {
